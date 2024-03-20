0166bf11901a711a2d57f04ea2b224f69427317c
wojciech <wmankowski@gmail.com>
2024-03-20 18:13:35 +0100

    add str to models.py

--- api/models.py
+++ api/models.py
@@ -5,6 +5,9 @@ class DisabilityType(Model):
     type = CharField(max_length=250)
     description = CharField(max_length=250)
 
+    def __str__(self):
+        return f"Typ: {self.type}"
+
 
 class Place(Model):
     name = CharField(max_length=250)
@@ -13,16 +16,21 @@ class Place(Model):
     url_map_google = URLField(max_length=250)
     disability_type_id = ForeignKey(DisabilityType, on_delete=CASCADE, related_name="DisabilityType")
 
-
+    def __str__(self):
+        return f'Miejsce o nazwie: {self.name}'
 class Comment(Model):
     content = CharField(max_length=250)
     user_name = CharField(max_length=250)
     date = DateTimeField(auto_now_add=True)
     place = ForeignKey(Place, on_delete=CASCADE, related_name='komentarze')
 
+    def __str__(self):
+        return f"Komentarz z dnia: {self.date}"
 
 class Rating(Model):
     score = FloatField()
     number_of_ratings = IntegerField()
     place = ForeignKey(Place, on_delete=CASCADE, related_name='ratings')
 
+    def  __str__(self):
+        return f"Ocena dla miejsca o id: {self.place}"
