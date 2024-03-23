aa468f7fa0146bb08e10d22773b17ffdbade9d1d
wojciech <wmankowski@gmail.com>
2024-03-23 18:00:36 +0100

    add CORS in settings.py

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
-from django.db.models import Model, ManyToManyField, CharField, DateTimeField, ForeignKey, IntegerField, URLField, CASCADE, FloatField
+from django.db.models import Model, ManyToManyField, CharField, DateTimeField, ForeignKey, IntegerField, URLField, \
+    CASCADE, FloatField
 
 
 class DisabilityType(Model):
@@ -18,6 +19,8 @@ class Place(Model):
 
     def __str__(self):
         return f'Miejsce o nazwie: {self.name}'
+
+
 class Comment(Model):
     content = CharField(max_length=250)
     user_name = CharField(max_length=250)
@@ -27,10 +30,11 @@ class Comment(Model):
     def __str__(self):
         return f"Komentarz z dnia: {self.date}"
 
+
 class Rating(Model):
     score = FloatField()
     number_of_ratings = IntegerField()
     place = ForeignKey(Place, on_delete=CASCADE, related_name='ratings')
 
-    def  __str__(self):
+    def __str__(self):
         return f"Ocena dla miejsca o id: {self.place}"
--- api_django/settings.py
+++ api_django/settings.py
@@ -7,10 +7,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["dostepnyswiatdlawszystkich.pythonanywhere.com", ]
 
-CORS_ALLOWED_ORIGINS = ["https://dostepnyswiatdlawszystkich.pythonanywhere.com"]
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGIN_REGEXES = ["https://dostepnyswiatdlawszystkich.pythonanywhere.com" ]
 INSTALLED_APPS = [
     'rest_framework',
     'django.contrib.admin',
@@ -91,3 +87,14 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 MEDIA_URL='/media/'
+
+CORS_ALLOWED_ORIGINS = [
+    'dostepnyswiatdlawszystkich.netlify.app', 'localhost:5173/'
+]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT']
+CORS_ALLOW_HEADERS = ['content-type', 'x-requested-with']
+
+
+
