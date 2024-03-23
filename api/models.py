187a8ce74dd401594d1829924d7ea0bafce8306f
wojciech <wmankowski@gmail.com>
2024-03-23 19:00:59 +0100

    add correct in serializer.py

--- api/models.py
+++ api/models.py
@@ -16,7 +16,7 @@ class Place(Model):
     url_imge = URLField(max_length=250)
     url_map_google = URLField(max_length=250)
     # disability_type_id = ManyToManyField(to=DisabilityType,related_name="places")
-
+    disability_type_id = ManyToManyField(DisabilityType, through='DisabilityTypePlace', related_name="places")
     def __str__(self):
         return f'Miejsce o nazwie: {self.name}'
 
